d6ed6fe97a8f4e916358e68ac2b79c63593b3ab6
Paulo <8630187+papawlo@users.noreply.github.com>
2020-08-07 21:15:19 +0100

    :rocket: desktop ready

--- src/app.js
+++ src/app.js
@@ -1,36 +1,7 @@
 const toggleMenu = document.getElementById('toggle-menu');
-const nav = document.getElementById('nav');
-const navStatus = false;
-
-
-// determines if the user has a set theme
-function detectColorScheme() {
-    let theme = "light";    //default to light
-
-    //local storage is used to override OS theme settings
-    if (localStorage.getItem("theme")) {
-        if (localStorage.getItem("theme") == "dark") {
-            theme = "dark";
-        }
-    } else if (!window.matchMedia) {
-        //matchMedia method not supported
-        return false;
-    } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-        //OS theme setting detected as dark
-        theme = "dark";
-    }
-
-    //dark theme preferred, set document with a `data-theme` attribute
-    if (theme == "dark") {
-        // document.documentElement.setAttribute("data-theme", "dark");
-        document.documentElement.classList.add('dark');
-        toggleSwitch.checked = true;
-    } else {
-        document.documentElement.classList.add('light');
-    }
-
-}
-
+const nav = document.getElementById('mobile-nav');
+const iconHamburger = document.getElementById('icon-hamburger');
+const iconClose = document.getElementById('icon-close');
 
 
 //function that changes the theme, and sets a localStorage variable to track the theme between page loads
@@ -38,8 +9,12 @@ function openMenu() {
 
     if (nav.classList.contains('open')) {
         nav.classList.remove('open');
+        iconHamburger.classList.remove('hide');
+        iconClose.classList.add('hide');
     } else {
         nav.classList.add('open');
+        iconHamburger.classList.add('hide');
+        iconClose.classList.remove('hide');
     }
 
 }
